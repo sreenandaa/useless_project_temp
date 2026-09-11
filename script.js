806df394d1c91e96ebbe7e6436a1fac6bb4b927e
Sreenanda <sreenandavh@gmail.com>
2026-09-11 19:28:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    padding-top: 100px;
+}
+
+h1 {
+    font-size: 40px;
+}
+
+button {
+    padding: 15px 30px;
+    font-size: 20px;
+    cursor: pointer;
+}
